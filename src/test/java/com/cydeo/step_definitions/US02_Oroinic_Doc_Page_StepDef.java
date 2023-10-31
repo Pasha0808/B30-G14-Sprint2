b3a548e6371e9bc2b07ac54d650114d704f51012
ninaCydeo <ninap9@icloud.com>
2023-10-31 12:08:37 -0400

    Feature file step def resources file

---
+++ src/test/java/com/cydeo/step_definitions/US02_Oroinic_Doc_Page_StepDef.java
@@ -0,0 +1,12 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US02_Oroinic_Doc_Page_StepDef {
+
+
+
+
+}
