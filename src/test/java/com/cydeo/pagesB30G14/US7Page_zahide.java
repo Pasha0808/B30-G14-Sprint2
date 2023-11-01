35c014c3136a441293ad7ad318c4ada44920b093
zahideren <erozgur80@hotmail.com>
2023-11-01 17:37:26 -0400

    update us7_zahide page class

---
+++ src/test/java/com/cydeo/pagesB30G14/US7Page_zahide.java
@@ -0,0 +1,36 @@
+package com.cydeo.pagesB30G14;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class US7Page_zahide {
+    public US7Page_zahide() {
+        PageFactory.initElements(Driver.getDriver(), this);
+
+}
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(id = "_submit")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "(//span[@class='title title-level-1'])[2]")
+    public WebElement fleetDropDownBtn;
+    @FindBy(xpath = "//a/span[.='Vehicles']")
+    public WebElement vehicleDropDownBtn;
+    @FindBy(xpath = "//button/input[@type='checkbox']")
+    public WebElement allCheckBoxBtn;
+
+    @FindBy(xpath = "(//input[@data-role='select-row-cell'])[1]")
+    public WebElement anyCarCheckBoxBtn;
+
+
+
+
+
+    }
