f2cc05fc75f3391230a9d9b8c8c3931ee9ad816e
zahideren <erozgur80@hotmail.com>
2023-11-01 17:38:22 -0400

    added codes us7_zahide step definition class

--- src/test/java/com/cydeo/step_definitions/US7_StepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/US7_StepDefinitions.java
@@ -1,6 +1,52 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class US7_StepDefinitions {
+
+
+    @Given("user is go to login page")
+    public void user_is_go_to_login_page() {
+
+    }
+    @Then("user enter username and password")
+    public void user_enter_username_and_password() {
+
+    }
+    @Then("user click login button")
+    public void user_click_login_button() {
+
+    }
+    @Then("user sees home page successfully")
+    public void user_sees_home_page_successfully() {
+
+    }
+    @Then("user {string} hover over the fleet module")
+    public void user_hover_over_the_fleet_module(String string) {
+
+    }
+    @Then("user {string} click the Vehicle module")
+    public void user_click_the_vehicle_module(String string) {
+
+    }
+    @Then("user {string} go to Vehicle page successfully")
+    public void user_go_to_vehicle_page_successfully(String string) {
+
+    }
+    @Then("user {string} check the all cars button already not selected")
+    public void user_check_the_all_cars_button_already_not_selected(String string) {
+
+    }
+    @Then("user {string} select  the all cars checkbox button can be able to select")
+    public void user_select_the_all_cars_checkbox_button_can_be_able_to_select(String string) {
+
+    }
+    @Then("user {string} select any car checekbox buttn")
+    public void user_select_any_car_checekbox_buttn(String string) {
+
+    }
+
 
 
 }
