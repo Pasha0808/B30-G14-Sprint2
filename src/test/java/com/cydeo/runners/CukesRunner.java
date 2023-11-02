3ad392a8ae28cc3fdb28326664600b7994d915b0
Pasha0808 <pavloromanyuk@yahoo.com>
2023-11-02 17:33:08 -0400

    pinbar update 2(snipets)

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/B30G14_feature_file",
         glue = "com/cydeo/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@B30G14-209",
         publish = true //generating a report with public link
 )
--- src/test/java/com/cydeo/step_definitions/US03_PinbarFunction.java
+++ src/test/java/com/cydeo/step_definitions/US03_PinbarFunction.java
@@ -1,4 +1,24 @@
 package com.cydeo.step_definitions;
 
+import io.cucumber.java.en.Then;
+
 public class US03_PinbarFunction {
+    @Then("user should click on Learn how to use this space")
+    public void user_should_click_on_learn_how_to_use_this_space() {
+
+    }
+    @Then("user should see an image on the page")
+    public void user_should_see_an_image_on_the_page() {
+
+    }
+
+    @Then("user should see How To Use Pinbar text")
+    public void user_should_see_how_to_use_pinbar_text() {
+
+    }
+    @Then("user should see Use the pin icon on the right top corner of the page to create fast access link in the pinbar text")
+    public void user_should_see_use_the_pin_icon_on_the_right_top_corner_of_the_page_to_create_fast_access_link_in_the_pinbar_text() {
+
+    }
+
 }
